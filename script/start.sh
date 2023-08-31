164aa2321f72db6d0bde81a7a74047b88447b66c
Woody_Cha <gkstls2006@naver.com>
2023-08-31 17:39:22 +0900

    빌드스펙추가

---
+++ script/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo su -
+cd /root/kokonut_batch/
+
+# 프로세스 종료
+# ps aux | grep java | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep java | grep -v sonarqube | grep -v grep | awk '{print $2}' | xargs kill
+
+# 10초간 대기합니다.
+sleep 10
+
+# 파일 삭제
+#rm /root/kokonut_backend/kokonut*.jar
+# 새로운 파일 복사
+cp /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/kokonut*.jar /root/kokonut_batch/kokonut*.jar
+
+source ~/.zshrc
+
+# 새로운 프로세스 시작
+mkdir /root/kokonut_batch/logs
+
+nohup java -jar -Dserver.port=8050 kokonut_batch-0.0.1-SNAPSHOT.jar > /root/kokonut_batch/logs/$(date +%Y-%m-%d).log 2>&1 &
+
+exit
